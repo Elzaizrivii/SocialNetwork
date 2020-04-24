7040e4f2a69a13f23ead154cf60493b568faebb6
Elzaizrivii <temirhan023@gmail.com>
2020-04-24 17:34:37 +0500

    -Рефакторинг модуля Диалог -Удалены ненужные зависимости

--- src/App.js
+++ src/App.js
@@ -17,11 +17,11 @@ const App = () => {
                 <Header/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
-                    <Route path='/Profile' component={Profile}/>
-                    <Route path='/Dialogs' component={Dialogs}/>
-                    <Route path='/News' component={News}/>
-                    <Route path='/Music' component={Music}/>
-                    <Route path='/Settings' component={Settings}/>
+                    <Route exact path='/Profile' component={Profile}/>
+                    <Route exact path='/Dialogs' component={Dialogs}/>
+                    <Route exact path='/News' component={News}/>
+                    <Route exact path='/Music' component={Music}/>
+                    <Route exact path='/Settings' component={Settings}/>
                 </div>
             </div>
         </BrowserRouter>
--- src/Dialogs/Dialogs.jsx
+++ src/Dialogs/Dialogs.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import s from "./Dialogs.module.css";
+import {NavLink} from "react-router-dom";
 
-const Dialogs = (props) =>{
-    return(
-        <div>
-            hello world
+const DialogsItem = (props) => {
+    let path = "/dialogs/" + props.id;
+    return (
+        <div className={s.dialog}>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    );
+};
+
+const Message = (props) => {
+    return (
+        <div className={s.message}>{props.message}</div>
+    );
+};
+
+const Dialogs = (props) => {
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <DialogsItem name="Temirhan" id="1"/>
+                <DialogsItem name="Aliya" id="2"/>
+                <DialogsItem name="Alihan" id="3"/>
+            </div>
+            <div className={s.messages}>
+                <Message message="hello"/>
+                <Message message="yo"/>
+                <Message message="Hi"/>
+
+            </div>
         </div>
     );
 };
